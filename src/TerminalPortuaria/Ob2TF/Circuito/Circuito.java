87ffb06e5ff96649d080c760373e767614c85506
Salles, Patricio <pato90_28@hotmail.com>
2023-10-29 20:48:55 -0300

    Agregado funcionalidades de tramo

---
+++ src/TerminalPortuaria/Ob2TF/Circuito/Circuito.java
@@ -0,0 +1,19 @@
+package TerminalPortuaria.Ob2TF.Circuito;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Circuito {
+
+	private List<Tramo> listaDeTramo = new ArrayList<Tramo>();
+
+	public List<Tramo> getListaDeTramo() {
+		return listaDeTramo;
+	}
+
+	public void setListaDeTramo(List<Tramo> listaDeTramo) {
+		this.listaDeTramo = listaDeTramo;
+	}
+	
+	
+}
---
+++ src/TerminalPortuaria/Ob2TF/Circuito/Tramo.java
@@ -0,0 +1,57 @@
+package TerminalPortuaria.Ob2TF.Circuito;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import TerminalPortuaria.Ob2TF.TerminalP.TerminalPortuaria;
+
+
+public class Tramo {
+	private TerminalPortuaria puertoOrigen;
+	private TerminalPortuaria puertoDestino;
+	private LocalDate fechaSalida;
+	private long duracionViaje;
+	private double costoDetramo;
+	
+	
+	
+	
+	public TerminalPortuaria getPuertoOrigen() {
+		return puertoOrigen;
+	}
+
+	public TerminalPortuaria getPuertoDestino() {
+		return puertoDestino;
+	}
+
+	public long getDuracionViaje() {
+		return duracionViaje;
+	}
+
+	public double getCostoDetramo() {
+		return costoDetramo;
+	}
+
+	public LocalDate getFechaSalida() {
+		return fechaSalida;
+	}
+
+	public void setFechaSalida(LocalDate fechaSalida) {
+		this.fechaSalida = fechaSalida;
+	}
+	
+	//PRIMERA FORMA
+//	public LocalDate fechaLlegada() {
+//		long diasDeViaje = (this.duracionViaje / 24);
+//		return this.fechaSalida.plusDays(diasDeViaje);
+//	}
+	
+	//SEGUNDA FORMA
+	
+	public LocalDate fechaLlegada() {
+		return this.fechaSalida.plus(this.duracionViaje, ChronoUnit.HOURS);
+	}
+	
+	public void main () {
+		System. out. println(this.fechaLlegada());
+	}
+
+}
