60d15475e4940e8b1dbdfdf0461ace1903f4fe5b
walterggomez <walterggomez@gmail.com>
2023-10-28 22:29:45 -0300

    Se crea el test para mejor ruta

---
+++ test/TerminalPortuariaOb2TF/EstrategiaMejorRutaTest/MejorRutaTest.java
@@ -0,0 +1,19 @@
+package TerminalPortuariaOb2TF.EstrategiaMejorRutaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MejorRutaTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
