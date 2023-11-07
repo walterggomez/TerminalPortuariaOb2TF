04d5a597af8fdb20f184be8d2ea54292335ef187
Evante <Franciscosalig@gmail.com>
2023-11-07 18:49:32 -0300

    Test navieras

--- src/TerminalPortuaria/Ob2TF/Circuito/Tramo.java
+++ src/TerminalPortuaria/Ob2TF/Circuito/Tramo.java
@@ -7,7 +7,7 @@ import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import TerminalPortuaria.Ob2TF.TerminalP.TerminalPortuaria;
 import java.time.format.DateTimeFormatter;
-
+// asd
 
 public class Tramo
 {
